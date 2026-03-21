c2bfe3161ff91d0856d426cb68ee76fec431ac1d
Gosen <g2294832549@gmail.com>
2026-03-21 20:54:10 +0800

    4

--- test_for_github/Assets/Scenes/test3.cs
+++ test_for_github/Assets/Scenes/test3.cs
@@ -7,7 +7,7 @@ public class test3 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("I dont want this.")
     }
 
     // Update is called once per frame
